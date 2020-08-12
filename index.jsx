147937ebdba76812d17d4ea48f93175035674c88
sunlingli <sunlingli@youzi-tech.cn>
2020-08-12 15:48:12 +0800

    1-1

---
+++ index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class index extends Component {
+    methodName = (params) => {
+        this.setState();
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default index;
