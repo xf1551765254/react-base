7a5791f283ba0d26c49c03fbdb680e4e3ef756ed
sunlingli <sunlingli@youzi-tech.cn>
2020-08-14 15:30:47 +0800

    react

--- project2/src/App.js
+++ project2/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './App.scss';
-import { Input, Button } from 'antd'
+import {
+  Input,
+  Button
+} from 'antd'
 import 'antd/dist/antd.css'
+
 
 class App extends React.Component {
   state = {
@@ -9,7 +13,10 @@ class App extends React.Component {
     list: []
   }
   handerAdd = () => {
-    let { val, list } = this.state
+    let {
+      val,
+      list
+    } = this.state
     list.push(val)
     this.setState({
       list
@@ -22,36 +29,58 @@ class App extends React.Component {
     });
   }
   render() {
-    const { val, list } = this.state
-    return (
-      <div className="App">
+    const {
+      val,
+      list
+    } = this.state
+    return ( <
+      div className = "App" >
 
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> 
-         */}
-        <h1>1233</h1>
-        <Input type="text" value={val} style={{ width: 300 }} onChange={this.handleChange} />
-        <Button onClick={this.handerAdd}>添加</Button>
-        <ul>
-          {
-            list.map((item, index) => {
-              return <li key={index}>{item}</li>
-            })
-          }
-        </ul>
-      </div>
+      {
+        /* <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                  </p>
+                  <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Learn React
+                  </a>
+                </header> 
+                 */
+      } <
+      h1 > 1233 < /h1> <
+      Input type = "text"
+      value = {
+        val
+      }
+      style = {
+        {
+          width: 300
+        }
+      }
+      onChange = {
+        this.handleChange
+      }
+      /> <
+      Button onClick = {
+        this.handerAdd
+      } > 添加 < /Button> <
+      ul > {
+        list.map((item, index) => {
+          return <li key = {
+            index
+          } > {
+            item
+          } < /li>
+        })
+      } <
+      /ul> <
+      /div>
     );
   }
 }
--- project2/src/pages/404.jsx
+++ project2/src/pages/404.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 export default function NoMacth() {
     return <div style={{ color: 'red', textAlign: 'center' }}>
-        404!!!
+        404
     </div>
 }
